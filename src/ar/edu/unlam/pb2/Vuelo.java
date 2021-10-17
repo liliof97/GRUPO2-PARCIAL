f79a8e1ea6300f687a2746435ffb5d298a8fb5a9
Lucas <lucasltorres1997@gmail.com>
2021-10-17 02:17:12 -0300

    Metodo precioPasaje y test con el metodo precioPasaje

--- src/ar/edu/unlam/pb2/Vuelo.java
+++ src/ar/edu/unlam/pb2/Vuelo.java
@@ -3,27 +3,27 @@ package ar.edu.unlam.pb2;
 public class Vuelo {
 
 	private Pasajero pasajeros[];
-	//Esto puede variar al hacer el test en JUnit
+	// Esto puede variar al hacer el test en JUnit
 	private final Integer CANT_MAXIMA_PASAJEROS = 3;
 	private Integer cantidadActualDePasajeros;
 	private Integer numeroDeVuelo;
-	
-	private Pasajero turista [][];
-	private Pasajero primeraClase [][];
+
+	private Pasajero turista[][];
+	private Pasajero primeraClase[][];
 	private Pasajero claseBusiness[][];
-	
+
 	public Vuelo(Integer numeroDeVuelo) {
 		pasajeros = new Pasajero[CANT_MAXIMA_PASAJEROS];
 		this.cantidadActualDePasajeros = 0;
-		this.numeroDeVuelo=numeroDeVuelo;
+		this.numeroDeVuelo = numeroDeVuelo;
 	}
-	
+
 	public Boolean agregarPasajero(Pasajero pasajero) {
-		Boolean seAgrego= false;
-		
-		if(this.cantidadActualDePasajeros<this.pasajeros.length) {
+		Boolean seAgrego = false;
+
+		if (this.cantidadActualDePasajeros < this.pasajeros.length) {
 			pasajeros[this.cantidadActualDePasajeros++] = pasajero;
-			seAgrego=true;
+			seAgrego = true;
 		}
 		return seAgrego;
 	}
@@ -35,7 +35,23 @@ public class Vuelo {
 	public void setNumeroDeVuelo(Integer numeroDeVuelo) {
 		this.numeroDeVuelo = numeroDeVuelo;
 	}
-	
-	
-}
 
+	public Integer precioPasaje(Pasajero pasajero) {
+		Integer precioDelPasaje = 0;
+		switch (pasajero.getTipo()) {
+		case PRIMERACLASE:
+			precioDelPasaje = 10000;
+			break;
+		case CLASEBUSINESS:
+			precioDelPasaje = 5000;
+			break;
+		case CLASETURISTA:
+			precioDelPasaje = 2500;
+			break;
+		default:
+			break;
+		}
+		return precioDelPasaje;
+	}
+
+}
--- test/ar/edu/unlam/pb2/VueloTest.java
+++ test/ar/edu/unlam/pb2/VueloTest.java
@@ -8,46 +8,65 @@ public class VueloTest {
 
 	@Test
 	public void queSePuedaAgregarUnPasajeroAUnVueloCorrectamente() {
-		String nombre="Mario";
-		String apellido="Lopez";
+		String nombre = "Mario";
+		String apellido = "Lopez";
 		Integer pasaporte = 23445;
 		Integer dni = 40001002;
 		Integer numeroDeVuelo = 1;
 		Boolean valorEsperado = true;
-		
+
 		Pasajero pasajero = new Pasajero(nombre, apellido, pasaporte, dni, TipoDeVuelo.CLASETURISTA);
-		
+
 		Vuelo vuelo = new Vuelo(numeroDeVuelo);
-		
+
 		Boolean valorObtenido = vuelo.agregarPasajero(pasajero);
-		
-		assertEquals(valorEsperado, valorObtenido);	
-		//Si no recuerdo mal también se puede verificar de esta forma
+
+		assertEquals(valorEsperado, valorObtenido);
+		// Si no recuerdo mal también se puede verificar de esta forma
 		assertTrue(valorObtenido);
 	}
-	
+
 	@Test
 	public void queAlSuperarLaCapacidadMaximaNoPermitaIngresarPasajero() {
-		String nombre="Mario";
-		String apellido="Lopez";
+		String nombre = "Mario";
+		String apellido = "Lopez";
 		Integer pasaporte = 23445;
 		Integer dni = 40001002;
 		Integer numeroDeVuelo = 1;
 		Boolean valorEsperado = false;
-		
+
 		Pasajero pasajero1 = new Pasajero(nombre, apellido, pasaporte, dni, TipoDeVuelo.CLASETURISTA);
 		Pasajero pasajero2 = new Pasajero(nombre, apellido, pasaporte, dni, TipoDeVuelo.CLASETURISTA);
 		Pasajero pasajero3 = new Pasajero(nombre, apellido, pasaporte, dni, TipoDeVuelo.CLASETURISTA);
 		Pasajero pasajero4 = new Pasajero(nombre, apellido, pasaporte, dni, TipoDeVuelo.CLASETURISTA);
-		
+
 		Vuelo vuelo = new Vuelo(numeroDeVuelo);
-		
+
 		Boolean valorObtenido = vuelo.agregarPasajero(pasajero1);
 		valorObtenido = vuelo.agregarPasajero(pasajero2);
 		valorObtenido = vuelo.agregarPasajero(pasajero3);
 		valorObtenido = vuelo.agregarPasajero(pasajero4);
-		
-		assertEquals(valorEsperado, valorObtenido);	
+
+		assertEquals(valorEsperado, valorObtenido);
+	}
+
+	@Test
+	public void queAlElegirTipoDeVueloPrimeraClaseElPrecioDelPasajeSeaDiezMil() {
+
+		String nombre = "Mario";
+		String apellido = "Lopez";
+		Integer pasaporte = 23445;
+		Integer dni = 40001002;
+		Integer numeroDeVuelo = 1;
+		Integer valorEsperado = 10000;
+
+		Pasajero pasajero = new Pasajero(nombre, apellido, pasaporte, dni, TipoDeVuelo.PRIMERACLASE);
+		Vuelo vuelo = new Vuelo(numeroDeVuelo);
+
+		Integer valorObtenido = vuelo.precioPasaje(pasajero);
+
+		assertEquals(valorEsperado, valorObtenido);
+
 	}
 
 }
