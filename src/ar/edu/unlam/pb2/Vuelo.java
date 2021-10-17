436fdafa8e3c1af36c6c6930d8469e8ea941e2f0
Jonathan Martins <jonathan_l_m@hotmail.com>
2021-10-17 17:55:17 -0300

    Se sube verificacion de destino y precio en la clase vuelo + test

--- src/ar/edu/unlam/pb2/Vuelo.java
+++ src/ar/edu/unlam/pb2/Vuelo.java
@@ -1,7 +1,9 @@
 package ar.edu.unlam.pb2;
 
 public class Vuelo {
-
+	
+	private DestinoVuelo destino;
+	private Integer valorDestino = 0;
 	private Pasajero pasajeros[];
 	//Esto puede variar al hacer el test en JUnit
 	private final Integer CANT_MAXIMA_PASAJEROS = 100;
@@ -35,6 +37,89 @@ public class Vuelo {
 	public void setNumeroDeVuelo(Integer numeroDeVuelo) {
 		this.numeroDeVuelo = numeroDeVuelo;
 	}
+	
+	public DestinoVuelo getDestino() {
+		return destino;
+	}
+	
+	public void setDestino(DestinoVuelo destino) {
+		this.destino = destino;
+	}
+	
+	public String verificarPrecioDestino(DestinoVuelo destino) {
+		switch(destino) {
+		case ITALIA:
+			valorDestino = 3400;
+			break;
+		case FRANCIA:
+			valorDestino = 3500;
+			break;
+		case ALEMANIA:
+			valorDestino = 3100;
+			break;
+		case ESPAÑA:
+			valorDestino = 3200;
+			break;
+		case RUSIA:
+			valorDestino = 4000;
+			break;
+		case INGLATERRA:
+			valorDestino = 4500;
+			break;
+		case NORUEGA:
+			valorDestino = 4700;
+			break;
+		case CHINA:
+			valorDestino = 6000;
+			break;
+		case JAPON:
+			valorDestino = 8000;
+			break;
+		case COREA:
+			valorDestino = 2500;
+			break;
+		}
+		return "El valor de su destino es de " + valorDestino;		
+	}
+	
+	public String verificarDestino(DestinoVuelo destino) {
+		switch(destino) {
+		case ITALIA:
+			destino = DestinoVuelo.ITALIA;
+			break;
+		case FRANCIA:
+			destino = DestinoVuelo.FRANCIA;
+			break;
+		case ALEMANIA:
+			destino = DestinoVuelo.ALEMANIA;
+			break;
+		case ESPAÑA:
+			destino = DestinoVuelo.ESPAÑA;
+			break;
+		case RUSIA:
+			destino = DestinoVuelo.RUSIA;
+			break;
+		case INGLATERRA:
+			destino = DestinoVuelo.INGLATERRA;
+			break;
+		case NORUEGA:
+			destino = DestinoVuelo.NORUEGA;
+			break;
+		case CHINA:
+			destino = DestinoVuelo.CHINA;
+			break;
+		case JAPON:
+			destino = DestinoVuelo.JAPON;
+			break;
+		case COREA:
+			destino = DestinoVuelo.COREA;
+			break;
+		}
+		return "El destino de su vuelo es " + destino;		
+	}
+
+	
+	
 	
 	
 }
--- test/ar/edu/unlam/pb2/VueloTest.java
+++ test/ar/edu/unlam/pb2/VueloTest.java
@@ -8,22 +8,39 @@ public class VueloTest {
 
 	@Test
 	public void queSePuedaAgregarUnPasajeroAUnVueloCorrectamente() {
-		String nombre="Mario";
-		String apellido="Lopez";
-		Integer pasaporte = 23445;
-		Integer dni = 40001002;
-		Integer numeroDeVuelo = 1;
+		
+		Pasajero pasajero = new Pasajero("Mario", "Lopez", 23445, 40001002, TipoDeVuelo.CLASETURISTA);
+		Vuelo vuelo = new Vuelo(1);
+		
 		Boolean valorEsperado = true;
-		
-		Pasajero pasajero = new Pasajero(nombre, apellido, pasaporte, dni, TipoDeVuelo.CLASETURISTA);
-		
-		Vuelo vuelo = new Vuelo(numeroDeVuelo);
-		
 		Boolean valorObtenido = vuelo.agregarPasajero(pasajero);
 		
 		assertEquals(valorEsperado, valorObtenido);	
 		//Si no recuerdo mal también se puede verificar de esta forma
 		assertTrue(valorObtenido);
 	}
+	
+	@Test
+	public void queSePuedaVerificarElDestino() {
+		
+		Vuelo vuelo = new Vuelo(1);
+		
+		String valorEsperado = "El destino de su vuelo es " + DestinoVuelo.CHINA;
+		
+		String valorObtenido = vuelo.verificarDestino(DestinoVuelo.CHINA);
+		
+		assertEquals(valorEsperado, valorObtenido);
+	}
 
+	@Test
+	public void queSePuedaVerificarElPrecioDelDestino() {
+		
+		Vuelo vuelo = new Vuelo(1);
+		
+		String valorEsperado = "El valor de su destino es de " + 3400;
+		
+		String valorObtenido = vuelo.verificarPrecioDestino(DestinoVuelo.ITALIA);
+		
+		assertEquals(valorEsperado, valorObtenido);
+	}
 }
